91ba9405e6a683cf428b04753acc2b9a32114824
Mark Mulder <markmulder@gmail.com>
2011-06-27 22:24:33 +0200

    Include the middleware in dummy app.

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -11,6 +11,8 @@ require "stopwatch"
 
 module Dummy
   class Application < Rails::Application
+    config.middleware.use "Rack::LoadSpeed"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
