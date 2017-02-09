61ee681a3511af4896b12b72dda730f490f227b4
Mark Mulder <markmulder@gmail.com>
2017-02-09 11:32:17 +0100

    Remove old specs

--- spec/dummy/app/controllers/welcome_controller.rb
+++
@@ -1,9 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-  end
-
-  def javascript_test
-    headers['Content-Type'] = 'application/x-javascript'
-    render :file => "#{Rails.root}/public/javascripts/test.js", :layout => false
-  end
-end
--- spec/dummy/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Dummy::Application.routes.draw do
-  root :to => "welcome#index"
-  match '/javascript_test', :to => "welcome#javascript_test"
-end
